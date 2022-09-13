bbadf736d49cd44d4e0afd89894915a149617cd1
Darian T. Yang <dty7@pitt.edu>
2022-09-13 12:27:17 -0400

    added dihedral analysis data

--- copy.sh
+++ copy.sh
@@ -13,7 +13,7 @@ for SYS in ${SYSTEMS[@]} ; do
     for VER in $(seq -f "%02g" 0 1 4) ; do
         mkdir v$VER
         rsync -axhvP dyang@ultron.structbio.pitt.edu:$SOURCE/$SYS/v$VER/$OUT_ROOT/*.dat v$VER/$OUT_ROOT/
-        rsync -axhvP dyang@ultron.structbio.pitt.edu:$SOURCE/$SYS/v$VER/$OUT_ROOT/*.pdb v$VER/$OUT_ROOT/
+        #rsync -axhvP dyang@ultron.structbio.pitt.edu:$SOURCE/$SYS/v$VER/$OUT_ROOT/*.pdb v$VER/$OUT_ROOT/
         #rsync -axhvP dyang@ultron.structbio.pitt.edu:$SOURCE/$SYS/v$VER/$OUT_ROOT/mmgbsa/*.dat v$VER/$OUT_ROOT/
     done
     cd ../..
