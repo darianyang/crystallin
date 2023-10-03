aeec69a61b71e7620d18bda566008f576afa560e
Darian T. Yang <dty7@pitt.edu>
2023-10-03 15:54:52 -0400

    made updated 25us allDb3d plot

--- data_plot_1D.py
+++ data_plot_1D.py
@@ -214,7 +214,7 @@ def plot_multiple_reps(dataname, ylim, ylabel, time_units=10**4, dist=(0,5,1),
         fig.savefig(f"figures/{savefig}", dpi=300, transparent=False)
     plt.show()
 
-def multi_rep_data(sys="nalld", dataname="o_angle.dat", replicates=(0,5)):
+def multi_rep_data(sys="nalld", dataname="o_angle.dat", replicates=(0,25)):
     data = [pre_processing(f"data/{sys}/v{i:02d}/1us/{dataname}")[1] 
                for i in range(replicates[0],replicates[1])]
     data = np.reshape(data, -1)
@@ -289,5 +289,5 @@ joint_plot()
 # plot_multiple_reps("o_angle.dat", (0,35), "Orientation Angle (°)", replicates=(0,5), 
 #                     window=10, time_units=10**3, linewidth=2, dist=(0,0.2,0.02))
 
-plt.show()
-#plt.savefig("figures/joint_25us_wt_allb3d.png", dpi=300, transparent=True)
+#plt.show()
+plt.savefig("figures/joint_25us_wt_allDb3d.png", dpi=300, transparent=True)
